89a8db04957a51f9f75d5a41a33574a0f6c2b559
hddigital-dev <53758993+hddigital-dev@users.noreply.github.com>
2023-12-04 10:40:34 +0700

    fixprompt

--- pages/_app.js
+++ pages/_app.js
@@ -1,45 +1,51 @@
+import { env } from '../next.config'
 import '../styles/globals.css'
-import { useEffect,useState } from 'react'
+import { useEffect, useState } from 'react'
 function MyApp({ Component, pageProps }) {
   const [isClient, setIsClient] = useState(false)
+  const [deferredPrompt, setDeferredPrompt] = useState(null)
   useEffect(() => {
-    console.log("HOME")
-    setIsClient(true)    
+    console.log("useEffect")
+    setIsClient(true);
+    const addToHomeScreenButton = document.getElementById('abc');
+    addToHomeScreenButton.addEventListener('click', () => {
+      // Show the install prompt
+      deferredPrompt.prompt();
+
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          alert("your app is added to home screen")
+        } else {
+          alert("You are rejected to add to home screen")
+        }
+
+        // Reset the deferredPrompt variable
+        addToHomeScreenButton.style.display = 'none';
+      });
+    });
     if ('serviceWorker' in navigator) {
+      console.log("serviceWorker in navigator")
       navigator.serviceWorker.register("./assets/services/service-worker.js")
         .then((registration) => {
-          
+          console.log('Service Worker registration successful:', registration);
           // Check if the beforeinstallprompt event is supported
           if ('onbeforeinstallprompt' in window) {
+            console.log("onbeforeinstallprompt in window")
             // Add an event listener to the beforeinstallprompt event
             window.addEventListener('beforeinstallprompt', (event) => {
               // Prevent the default behavior
+              console.log("beforeinstallprompt event")
               event.preventDefault();
-
-              // Store the event for later use
-              const deferredPrompt = event;
+              
+              setDeferredPrompt(envt)
 
               // Show your custom "Add to Home Screen" button
-              const addToHomeScreenButton = document.getElementById('abc');
+              
               addToHomeScreenButton.style.display = 'block';
 
               // Add an event listener to the button
-              addToHomeScreenButton.addEventListener('click', () => {
-                // Show the install prompt
-                deferredPrompt.prompt();
 
-                // Wait for the user to respond to the prompt
-                deferredPrompt.userChoice.then((choiceResult) => {
-                  if (choiceResult.outcome === 'accepted') {
-                    alert("your app is added to home screen")
-                  } else {
-                    alert("You are rejected to add to home screen")
-                  }
-
-                  // Reset the deferredPrompt variable
-                  addToHomeScreenButton.style.display = 'none';
-                });
-              });
             });
           }
           else {
@@ -51,8 +57,7 @@ function MyApp({ Component, pageProps }) {
         });
     }
 
-  }
-  )
+  }, [])
   return <Component {...pageProps} />
 }
 
